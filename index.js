80329858ceb16515cb65f12e02af6712b14e58ba
github.Sherareisha <sherareisha78@gmail.com>
2023-10-19 13:45:37 +0700

    tambah

--- index.js
+++ index.js
@@ -3,24 +3,24 @@ const express = require('express')
     const app = express()
     const port = 11101
 
-    // app.get('/',(req,res) =>{
-    //     res.send("Hello ini express")
-    // })
-    // app.get('/',(req,res) =>{
-    //     res.send("Hello Shera")
-    // })
-
-    app.get('/Ayam',(req,res) =>{
-        res.send("Ayam Goreng")
-    })
-    app.post('/Ayam',(req,res) =>{
-        res.send("Ayam Geprek")
-    })
-    app.put('/Ayam',(req,res) =>{
-        res.send("Ayam Bakar")
-    })
-    app.delete('/Ayam',(req,res) =>{
-        res.send("Ayam Sudah Habis")
-    })
-
+    app.get("/Ayam", (req, res) => {
+        res.json({
+            message: "Ayam Goreng",
+        });
+    });
+    app.post("/Ayam", (req, res) => {
+        res.json({
+            message: "Ayam Geprek",
+        });
+    });
+    app.put("/Ayam", (req, res) => {
+        res.json({
+            message: "Ayam Bakar",
+        });
+    });
+    app.delete("/Ayam", (req, res) => {
+        res.json({
+            message: "Ayam Sudah Habis",
+        });
+    });
     app.listen(port,() => console.log(`Server running on port ${port}`))
